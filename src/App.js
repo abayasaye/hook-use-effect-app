23ae5e33ec5ee00508255ff4fb5adf939f66ac55
abayasaye <abay400@gmail.com>
2022-08-24 10:31:05 +0300

    n

--- src/App.js
+++ src/App.js
@@ -9,51 +9,51 @@ import Desk from './components/pages/Desk';
 const changeArray = [1,2,3]
 const App = ()=>{
   const [change,setChange]=useState(1)
-  // const [pageSwap,setPageSwap]=useState("Game")
+  const [pageSwap,setPageSwap]=useState("Game")
 
-// const PagesChanger = ()=>{
-//   switch (pageSwap) {
-//     case "Game":
+const PagesChanger = ()=>{
+  switch (pageSwap) {
+    case "Game":
       
-//     return <Game/>;
-//     case "Football":
+    return <Game/>;
+    case "Football":
       
-//     return <Football/>;
-//     case "Basketball":
+    return <Football/>;
+    case "Basketball":
       
-//     return <Basketball/>;
+    return <Basketball/>;
   
-//     default:
-//       break;
-//   }
-// }
+    default:
+      break;
+  }
+}
 
-  // const SwitchPages = ()=>{
-  //     switch (change) {
-  //       case 1:
-  //         return <Game/>;
-  //       case 2:
-  //       return <Football/>;
-  //       case 3:
-  //         return <Basketball/>;
-  //       default:
-  //         break;
-  //     }
-  // }
+  const SwitchPages = ()=>{
+      switch (change) {
+        case 1:
+          return <Game/>;
+        case 2:
+        return <Football/>;
+        case 3:
+          return <Basketball/>;
+        default:
+          break;
+      }
+  }
 return(
   <div>
-    {/* <h1>COMPONENTS</h1>
+    <h1>COMPONENTS</h1>
     <h1>{change}</h1>
     <button onClick={()=>setChange(1)}>CLICK-TO-1</button>
     <button onClick={()=>setChange(2)}>CLICK-TO-2</button>
     <button onClick={()=>setChange(3)}>CLICK-TO-3</button><br/>
     <SwitchPages/>
   <hr/>
-    <button onClick={()=>{setPageSwap("Game")}} >CHANGE-TO-GAME</button>
-    <button onClick={()=>{setPageSwap("Football")}} >CHANGE-TO-FOOTBALL</button>
-    <button onClick={()=>{setPageSwap("Basketball")}} >CHANGE-TO-BASKETBALL</button> */}
+    <button onClick={()=>setPageSwap("Game")} >CHANGE-TO-GAME</button>
+    <button onClick={()=>setPageSwap("Football")} >CHANGE-TO-FOOTBALL</button>
+    <button onClick={()=>setPageSwap("Basketball")} >CHANGE-TO-BASKETBALL</button>
     {/* <button onClick={changeUp}>CHANGE-NUMBER</button> */}
-    {/* <PagesChanger/> */}
+    <PagesChanger/>
     <hr/>
     <Router count={change}/>
     {
