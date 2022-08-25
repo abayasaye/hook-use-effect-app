29015237d887f6395e402372474f582b78a2c08c
abayasaye <abay400@gmail.com>
2022-08-25 15:12:38 +0300

    n

--- src/components/pages/Game/Game.jsx
+++ src/components/pages/Game/Game.jsx
@@ -1,45 +1,50 @@
 import "./game.css";
 import React, { useEffect } from "react";
 import { useState } from "react";
-const c=console.log;
-const date = new Date()
+import { useContext } from "react";
+import {myContext} from "../Router/Router";
+
+const c = console.log;
+const date = new Date();
 function Game() {
-const [level,setLevel]=useState(0)
+  const newText = useContext(myContext);
 
-const [gameTime,setGameTime]=useState({})
+  const [level, setLevel] = useState(0);
 
-useEffect(()=>{
-  // alert("Welcome To The Game")
-},[])
+  const [gameTime, setGameTime] = useState({});
 
-useEffect(()=>{
-  // c("Start The Game")
-},[level])
+  useEffect(() => {
+    // alert("Welcome To The Game")
+  }, []);
 
-useEffect(()=>{
-setGameTime(new Date())
-},[])
-useEffect(()=>{
+  useEffect(() => {
+    // c("Start The Game")
+  }, [level]);
+
+  useEffect(() => {
+    setGameTime(new Date());
+  }, []);
+  useEffect(() => {
     // alert("the game is over")
-  },[gameTime])
-  
+  }, [gameTime]);
 
-const levelUp = ()=>{
-  setLevel(level+1)
-}
+  const levelUp = () => {
+    setLevel(level + 1);
+  };
 
-const changeGameTime = ()=>{
-  setGameTime(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
-}
+  const changeGameTime = () => {
+    setGameTime(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+  };
   return (
     <div className="game">
       <h1>GAME-COMPONENT</h1>
+      {c(newText)}
       <h1>{level}</h1>
       <button onClick={levelUp}>LEVEL-UP</button>
       <button onClick={changeGameTime}>change-time</button>
       <h1>{}</h1>
     </div>
   );
-};
+}
 
 export default Game;
--- src/components/pages/Router/Router.jsx
+++ src/components/pages/Router/Router.jsx
@@ -1,21 +1,34 @@
 import "./router.css";
 import React from "react";
-import App from "../../../App";
+// import App from "../../../App";
 import Game from "../Game";
 import Football from "../Football";
 import Basketball from "../Basketball";
 import { Routes, Route } from "react-router-dom";
 import PageNotFound from "../page-not-found/Page-not-found";
 
+import { useEffect } from "react";
+import { createContext } from "react";
+import { useState } from "react";
+const c = console.log;
+export const myContext = createContext();
 function Router() {
+  const [devices, setDevices] = useState([]);
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/Jeck99/fake-server/devices")
+      .then((res) => res.json())
+      .then((result) => setDevices(result));
+  }, []);
   return (
     <div className="router">
-      <Routes>
-        <Route path="/Game" element={<Game />}></Route>
-        <Route path="/Football" element={<Football />}></Route>
-        <Route path="/Basketball" element={<Basketball />}></Route>
-        <Route path="*" element={<PageNotFound />}></Route>
-      </Routes>
+      <myContext.Provider value={{ devices, setDevices }}>
+        <Routes>
+          <Route path="/Game" element={<Game />}></Route>
+          <Route path="/Football" element={<Football />}></Route>
+          <Route path="/Basketball" element={<Basketball />}></Route>
+          <Route path="*" element={<PageNotFound />}></Route>
+        </Routes>
+      </myContext.Provider>
     </div>
   );
 }
